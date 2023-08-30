774fd00990a107794b0b4867f439820b04fd459e
Gabriel Nonnemacher <NonnemacherG@delagelanden.com>
2023-08-30 16:42:27 -0300

    Adicionando properties do SonarCloud

---
+++ app/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Começando o APP</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgb(255, 254, 227)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
